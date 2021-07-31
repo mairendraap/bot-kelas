0d13905441a04438a705c192bf399642dad1f16c
mairendraap <81284728+mairendraap@users.noreply.github.com>
2021-07-31 16:07:26 +0700

    Add files via upload

---
+++ commands/absen30.js
@@ -0,0 +1,24 @@
+const { ID_CHANNEL, TAG, ID_ROLE , ID_ROLES } = require('../config30.json');
+
+module.exports = {
+    name: '30',
+    description: 'Ini Coding register',
+    async execute(message, args, client) {
+        if(message.channel.type == "dm") return message.channel.send("Kamu tidak bisa registrasi dari DM.");
+       if (message.channel.id != `${ID_CHANNEL}`) return message.channel.send(`Kamu bisa menggunakan command ini di <#${ID_CHANNEL}>`);
+       
+
+        const nickname = args.join(" ");
+        const private = client.channels.cache.get(`${ID_CHANNEL}`);
+
+        try {
+            message.member.roles.add(`${ID_ROLE}`);
+            message.member.roles.add(`${ID_ROLES}`);
+            message.member.setNickname(`${TAG}`);
+            private.send(`${message.author} _ACCEPTED_, __**${TAG}**__ Sebagai Siswa di _${message.guild.name}._`);
+        } catch(error) {
+            console.error(error); 
+        }
+    },
+};  
+
---
+++ commands/absen4.js
@@ -0,0 +1,24 @@
+const { ID_CHANNEL, TAG, ID_ROLE , ID_ROLES , CLASS_ID } = require('../config4.json');
+
+module.exports = {
+    name: '4',
+    description: 'Ini Coding register',
+    async execute(message, args, client) {
+        if(message.channel.type == "dm") return message.channel.send("Kamu tidak bisa registrasi dari DM.");
+       if (message.channel.id != `${ID_CHANNEL}`) return message.channel.send(`Kamu bisa menggunakan command ini di <#${ID_CHANNEL}>`);
+      
+        const nickname = args.join(" ");
+        const private = client.channels.cache.get(`${ID_CHANNEL}`);
+
+        try {
+            message.member.roles.add(`${ID_ROLE}`);
+            message.member.roles.add(`${ID_ROLES}`);
+            message.member.roles.add(`${CLASS_ID}`);
+            message.member.setNickname(`${TAG}`);
+            private.send(`${message.author} _ACCEPTED_, __**${TAG}**__ Sebagai __Ketua Kelas__ di _${message.guild.name}._`);
+        } catch(error) {
+            console.error(error); 
+        }
+    },
+};  
+
